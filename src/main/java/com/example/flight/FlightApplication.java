1063c2c95306d27c3c96afd9b5c6fe89af87cabd
Ged Tajonera <ged.mystyle@gmail.com>
2021-08-06 15:57:05 +0800

    code cleanup

--- src/main/java/com/example/flight/FlightApplication.java
+++ src/main/java/com/example/flight/FlightApplication.java
@@ -1,54 +1,16 @@
 package com.example.flight;
 
-import java.nio.charset.Charset;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.Month;
-import java.util.Date;
-import java.util.List;
-import java.util.Random;
-
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import javax.persistence.PersistenceContext;
-import javax.persistence.PersistenceUnit;
-import javax.persistence.spi.PersistenceUnitInfo;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityFilterAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScans;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-import com.example.entities.Flight;
-import com.example.entities.Payment;
-import com.example.flight.configurations.SecurityConfig;
-import com.example.repositories.FlightRepository;
-
-import net.bytebuddy.build.ToStringPlugin.Exclude;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class, SecurityFilterAutoConfiguration.class})
 @EntityScan(basePackages = "com.example.entities")
 @ComponentScan(basePackages = {"com.example.flight", "com.example.repositories"})
-//@EnableJpaRepositories(basePackages = "com.example.repositories")
 public class FlightApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
