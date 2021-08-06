48775074c893836a57987b9bd6f325d5d32ec2c5
Ged Tajonera <ged.mystyle@gmail.com>
2021-08-06 15:16:54 +0800

    code cleanup

--- src/main/java/com/example/entities/PaymentTransaction.java
+++ src/main/java/com/example/entities/PaymentTransaction.java
@@ -2,10 +2,14 @@ package com.example.entities;
 
 import java.util.UUID;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.GenericGenerator;
 
 @Entity
 public class PaymentTransaction {
